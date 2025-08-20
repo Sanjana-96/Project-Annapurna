c41d0b503bb751cf6c7ee62bc3a10407fb443cf9
Kondala Sanjana <sanjanakondala52@gmail.com>
2025-08-20 18:48:10 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,11 +2,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'annapurna',
-    password: 'Sanju@2004',
-    port: 5432,
+      connectionString: process.env.DATABASE_URL,  // ✅ use Render-provided URL
+  ssl: {
+    rejectUnauthorized: false,  // ✅ needed for Render
+  },
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'annapurna',
+    // password: 'Sanju@2004',
+    // port: 5432,
 });
 
 module.exports = pool;
