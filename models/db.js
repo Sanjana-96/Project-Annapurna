5983a0373517bbdc2f334755b809c777c27c8c42
Kondala Sanjana <sanjanakondala52@gmail.com>
2025-08-22 18:33:04 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -13,4 +13,12 @@ const pool = new Pool({
     // port: 5432,
 });
 
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error("❌ DB connection error:", err);
+  } else {
+    console.log("✅ DB connected:", res.rows[0]);
+  }
+});
+
 module.exports = pool;
